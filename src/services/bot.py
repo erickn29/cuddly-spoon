6acc0654be10c50a8beff5e4cd4c7c9151053d80
ya_novikov <arhdomain@gmail.com>
2024-08-28 22:26:17 +0300

    bot service & cache

--- src/services/bot.py
+++ src/services/bot.py
@@ -1,3 +1,6 @@
+from api.v1.bot_user.crud.schema import BotCreateInputSchema, BotUpdateDataSchema
+from models.bot import Bot
+from pydantic import UUID4
 from repositories.bot import BotRepository
 from services.base import BaseService
 from utils.cache import cache
@@ -6,9 +9,29 @@ from utils.cache import cache
 class BotService(BaseService):
     repository: BotRepository = BotRepository()
 
+    async def create(self, schema: BotCreateInputSchema) -> Bot | None:
+        if bot := await self.repository.create(schema):
+            if bot.is_active and not bot.is_stopped:
+                await self.bot_activate(bot.phone)
+            return bot
+        return
+
+    async def update(self, obj_id: UUID4, data: BotUpdateDataSchema) -> Bot | None:
+        if bot := await self.repository.update(obj_id, data):
+            if bot.is_stopped:
+                await self.bot_deactivate(bot.phone)
+            return bot
+        return
+
+    async def delete(self, obj_id: UUID4) -> UUID4 | None:
+        bot = await self.repository.get(obj_id)
+        if bot:
+            await self.bot_deactivate(bot.phone)
+            return await self.repository.delete(obj_id)
+        return
+
     @staticmethod
     async def bot_deactivate(phone: str):
-        print("start bot deactivate")
         phones = await cache.get("bot:active:phones")
         if phones:
             phones = phones.split(",")
@@ -19,11 +42,9 @@ class BotService(BaseService):
                     ",".join(phones),
                     60 * 60 * 24 * 30,
                 )
-                print("bot deactivated")
 
     @staticmethod
     async def bot_activate(phone: str):
-        print("start bot activate")
         phones = await cache.get("bot:active:phones")
         if phones:
             phones = phones.split(",")
@@ -34,4 +55,5 @@ class BotService(BaseService):
                     ",".join(phones),
                     60 * 60 * 24 * 30,
                 )
-                print("bot activated")
+        else:
+            await cache.set("bot:active:phones", phone, 60 * 60 * 24 * 30)
